0480842aac394e442c11b46dc7a449658c901c83
Zubin Mukherjee <zubin.mukherjee@mindinventory.com>
2021-01-19 12:41:48 +0530

    Components and Props - (functional)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+// import {Welcome} from './components/WelcomeComponent'
+
+const Welcome = (props) => (
+    <div><h1>Hello, {props.name}</h1></div>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      Testss
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">Parent</div>
+            <Welcome name="Sara"/>
+            <Welcome name="Cahal"/>
+            <Welcome name="Edite"/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/WelcomeComponent.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+// function Welcome(props) {
+//     return <h1>Hello, {props.name}</h1>;
+// }
+
+export class Welcome extends Component {
+    render() {
+        return (
+            <div>
+                Inside welcome component
+            </div>
+        );
+    }
+}
+//
+// export default Welcome;
